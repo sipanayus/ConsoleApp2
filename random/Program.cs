b418de1d8d4845e4dfd01d23ba96978f15ca0509
sipan <sipan@MY-BOOK>
2021-01-29 23:58:33 +0300

    Add project files.

---
+++ random/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kullanıcı = "Ev, İş Yeri, Fabrika";
+            int kW;
+            
+            Console.WriteLine("kullanıcı giriniz:");
+        }
+    }
+}
